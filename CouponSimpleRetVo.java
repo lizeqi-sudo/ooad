9aae1d9596f0caea54f7fe96f75042ac1935c441
lizeqi-sudo <53214386+lizeqi-sudo@users.noreply.github.com>
2020-12-24 11:55:30 +0800

    Update CouponSimpleRetVo.java
    --story=1001333 --user=李泽奇 优惠活动 https://www.tapd.cn/41199658/s/1002428

--- CouponSimpleRetVo.java
+++ CouponSimpleRetVo.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 public class CouponSimpleRetVo implements Serializable {
    private String couponSn;
 
-
     public CouponSimpleRetVo(Coupon po) {
         this.couponSn = po.getCouponSn();
     }
